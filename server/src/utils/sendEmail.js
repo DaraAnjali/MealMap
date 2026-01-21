c3b4b70db03068706b073235c62f0917459f4113
AnjaliDara <daraanjali07@gmail.com>
2026-01-21 22:54:38 +0530

    25 commit

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,18 +1,18 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // MUST be false for 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
+export const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
-    from: `"MealMap Support" <${process.env.SMTP_USER}>`,
+    from: `"MealMap" <${process.env.SMTP_USER}>`,
     to,
     subject,
     html,
